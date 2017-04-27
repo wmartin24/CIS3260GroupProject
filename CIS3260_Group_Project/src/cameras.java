46ca213e663ffb1985842c33fb6d6c32f1281d4d
wmartin24 <wmartin23@student.gsu.edu>
2017-04-26 23:49:09 -0400

    Update cameras.java

--- CIS3260_Group_Project/src/cameras.java
+++ CIS3260_Group_Project/src/cameras.java
@@ -1,8 +1,39 @@
 
-public class cameras {
+public class Cameras {
 
-	public static void main(String[] args) 
-	{
-
-	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 }
+ String Brand = "Sony";
+ String Color = "Black";
+ int screenSize=6;
+ final int resolution=1080;
+	boolean isFlashOn; 
+	int numberOfUSBInputs;
+	double price;
+	
+	boolean status; 
+	
+
+    void turnOn(){
+		this.status = true;
+		System.out.println("Camera Flash is On");
+    }
+		
+	void turnOff(){
+			this.status = false;
+			System.out.println("Camera Flash is off");
+		}
+	
+     void turnflashOn(){
+		this.status = true;
+		System.out.println("Camera Flash is On");
+	}
+	
+	void turnflashOff(){
+		this.status = false;
+		System.out.println("Camera Flash is off");
+	}
+
+}
+
